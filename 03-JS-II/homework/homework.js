a1f6b915a76dced2685d80db9972f4db560f6d32
Juan Ramon Estrada <juanramonestradav@gmail.com>
2022-07-19 05:41:35 -0500

    guardando homework js2 y js3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -165,19 +165,16 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if(numero == 1 || numero == 4 || numero == 0){
-    return false;
-  }else{
-    for (let i = 0; i < numero / 2; i++) {
-      if(numero % i == 0){
-        return false;
-      }
+  var temp = numero;
+  for (var i = 2; i <= numero; i++) {
+    if(numero === 1 ||numero % i === 0){
+      return false;
+    }else if(numero ===1){
+      return false;
+    }else {
+      return true;
     }
   }
-  if(numero % 2 == 0){
-    return true;
-  }
-  
 }
 
 function esVerdadero(valor){
@@ -216,6 +213,8 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var contador = 0;
+  
 }
 
 
--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,26 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  var result = 0;
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    result = element;
+  }
+  return result;
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
@@ -23,6 +31,12 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var temp = [];
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    temp.push(element + 1);
+  }
+  return temp;
 }
 
 
@@ -30,6 +44,9 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+
+  return array;
 }
 
 
@@ -38,6 +55,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
 
 
@@ -47,6 +66,15 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  var frase = '';
+  
+  for (const key in palabras) {
+    if (Object.hasOwnProperty.call(palabras, key)) {
+      const element = palabras[key];
+      frase += element + ' ';
+    }
+  }
+  return frase.trim();
 }
 
 
@@ -54,6 +82,7 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  return false;
 }
 
 
